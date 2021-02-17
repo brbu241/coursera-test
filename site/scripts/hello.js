f36593207335ad6229eafce72906ed0bf4930e5f
Barney Ji <brbu241@gmail.com>
2021-02-18 00:01:01 +0800

    add js

--- site/scripts/hello.js
+++ site/scripts/hello.js
@@ -3,9 +3,11 @@ x = 2;
 y = 3;
 document.write("<br>" +x+"+" +y+ "=" +(x+y) ); //結果
 
-var a = prompt("請輸入數字","0");
-if (a >= 0) {
-    document.write("正喔");
-}else{
-    document.write("負啦")
-}
+// 計算機小工具
+// var a = prompt("請輸入數字","0");
+// if (a >= 0) {
+//     document.write("正喔");
+// }else{
+//     document.write("負啦")
+// }
+
